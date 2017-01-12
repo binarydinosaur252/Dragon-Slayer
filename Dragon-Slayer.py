ee317998d7f0679177b9e481ed530c1004cb7a07
Cyrus B <cyrusb252@gmail.com>
2017-01-12 10:39:10 -0500

    Add files via upload

---
+++ Dragon-Slayer.py
@@ -0,0 +1,23 @@
+import random
+from time import sleep
+
+print "DRAGON SLAYER"
+print "designed and developed by Cyrus Burt and Eric Matt" + "\n"
+sleep(5)
+
+#game intro
+print "Welcome to the magical world of Kraagnord!"
+sleep(5)
+print "A strange and mystical world crawling with evil wizards, brave warriors and dragons!" + "\n"
+
+#rules
+print "and now for the rules."
+sleep(3)
+print "use the command Fight to slay evil creatures (but don't kill the good guys!)"
+sleep(3)
+print "use the command Pickup to take treasure and pick up things." + "\n\n\n\n\n" 
+
+#game start
+name = raw_input("What is your name?: ")
+name = str(name)
+print "greetings, " + name + " I have a quest for you" + "\n"
