9e4c6e480115e87fef13e3a8c2ff524e2e4d1b6e
binarydinosaur252 <cyrusb252@gmail.com>
2017-01-19 10:24:03 -0500

    fixed code to work in python 3

--- Dragon-Slayer.py
+++ Dragon-Slayer.py
@@ -21,7 +21,7 @@ sleep(3)
 print ("use the command Look to see what is in the room or look at specific items in the room" + "\n\n\n\n\n")
 
 #game start
-name = raw_input("What is your name?: ")
+name = input("What is your name?: ")
 name = str(name)
 print ("greetings, " + name + " I have a quest for you" + "\n")
 
@@ -30,16 +30,22 @@ print ("here are your weapons" + "\n\n")
 print ("YOU GOT A SWORD!")
 sleep(2)
 
-todo1 = raw_input("what do you do now? : ")
-if todo1 == ("Explore"):
-   print ("you look around and the wizard who gave you the sword" + "\n" "seems to have dissapeared")
-elif todo1 == ("Fight"):
-    print ("You swing your sword and don't hit anything")
-elif todo1 == ("Get"):
-    print ("You get 50 peices of gold that were hidden under a rock!")
-elif todo1 == ("Look"):
-    print ("You see stuff in the room!")
-else :
-    print ("I don't know how to " +todo1+ " because my programers didn't want to spend the time to make that work")
+leave = "n"
+while leave != "y":
+   todo1 = input("what do you do now? : ")
+   if todo1 == ("Explore"):
+      print ("you look around and the wizard who gave you the sword" + "\n" "seems to have dissapeared")
+      leave = input("do you want to leave y/n: ")
+   elif todo1 == ("Fight"):
+      print ("You swing your sword and don't hit anything")
+   elif todo1 == ("Get"):
+      print ("You get 50 peices of gold that were hidden under a rock!")
+   elif todo1 == ("31415")
+      print ("YOU WIN")
+   elif todo1 == ("Look"):
+      print ("You see stuff in the room!")
+   else :
+      print ("I don't know how to " +todo1+ " because my programers didn't want to spend the time to make that work")
+print ("You walk outside and see the a little town busy with people")
 
-
+print
