ef8a6ce4d7fa3415cb0f1679360563866c7918b5
Eric Matt <erichmatt@gmail.com>
2017-01-19 10:23:48 -0500

    Added variables and stuff

--- dungeon.py
+++ dungeon.py
@@ -25,10 +25,13 @@ room22 = "room22"
 room23 = "room23"
 room24 = "room24"
 room25 = "room25"
-class room():
-    stuff = "none"
+class room:
+    def room(n,e,s,w,items):
+        stuff = "Really cool stuff",items
+        doors = [["north",n],["east",e],["south",s],["west",w]]
+        return [doors,stuff]
 
-class level:
+class levels:
     def rooms(x,y):
         rooms = [[room1,room2,room3,room4,room5],
                  [room6,room7,room8,room9,room10],
@@ -37,4 +40,7 @@ class level:
                  [room21,room22,room23,room24,room25]]
         return rooms[x][y]
 
-print (level.rooms(1,1))
+print (levels.rooms(4,4))
+room1 = room.room(0,1,1,1,"Sword")
+
+print (room1[1])
