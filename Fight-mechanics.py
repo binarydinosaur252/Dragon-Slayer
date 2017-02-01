625b9341f0cf11ef8f20abc46fa4a1782957395c
binarydinosaur252 <cyrusb252@gmail.com>
2017-02-01 15:19:08 -0500

    Added a mechanism for fighting.

---
+++ Fight-mechanics.py
@@ -0,0 +1,9 @@
+
+a = 0
+while a != 3:
+    todo1 = input("What do you do now: ")
+    if todo1 == ("Fight"):
+        print("You hit the goblin")
+        a = a + 1
+    if a == 3:
+        print("you killed the goblin")
